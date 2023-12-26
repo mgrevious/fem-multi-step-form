612902c0615d54d62f67a20c3cdd0fad32c4cd29
Monica Grevious <mag.amtrust@gmail.com>
2023-12-26 18:44:53 -0500

    commented out old base path.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route
-        path="/fem-multi-step-form"
+        path="/fem-multi-step-form" // path="/"
         element={<RootLayout />}
         errorElement={<ErrorBoundary />}
       >
