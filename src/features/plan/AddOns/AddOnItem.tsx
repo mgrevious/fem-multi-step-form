8ce6aa5b1b3e8c4a412f1a3f93d862018bb1410b
Monica Grevious <mag.amtrust@gmail.com>
2023-12-27 12:37:17 -0500

    Added click event to addons rather than using checkbox onChange

--- src/features/plan/AddOns/AddOnItem.tsx
+++ src/features/plan/AddOns/AddOnItem.tsx
@@ -14,10 +14,22 @@ const AddOnItem: React.FC<Props> = ({ amount, name, description, header }) => {
   const { addOns, isYearly } = useAppSelector((state) => state.plan);
   const isChecked = addOns[name].checked;
   return (
-    <div
-      className={`p-4 flex mb-4 border rounded-md w-full hover:border-deep-purple ${
+    <button
+      id={`${name}-btn`}
+      name={name}
+      className={`p-4 flex items-center mb-4 border rounded-md w-full hover:border-deep-purple ${
         isChecked ? "bg-light-gray border-deep-purple" : "border-gray-300"
       }`}
+      onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+        dispatch(
+          setAddOn({
+            name: e.currentTarget.name,
+            checked: !isChecked,
+            header,
+            amount,
+          })
+        );
+      }}
     >
       <div className="mr-4 flex items-center">
         <input
@@ -27,12 +39,7 @@ const AddOnItem: React.FC<Props> = ({ amount, name, description, header }) => {
           name={name}
           type="checkbox"
           className="h-4 w-4 rounded border-deep-purple text-deep-purple focus:ring-deep-purple hover:cursor-pointer"
-          onChange={(e) => {
-            const addOn = e.target.name.replace(" ", "");
-            dispatch(
-              setAddOn({ name: addOn, checked: !isChecked, header, amount })
-            );
-          }}
+          onChange={() => {}}
         />
       </div>
       <div className="flex justify-between flex-1">
@@ -50,7 +57,7 @@ const AddOnItem: React.FC<Props> = ({ amount, name, description, header }) => {
           {}
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
