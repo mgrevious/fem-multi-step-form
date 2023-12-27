a1bbacbf68d6a48928c83602c11336b73dabfc9a
Monica Grevious <mag.amtrust@gmail.com>
2023-12-27 01:14:53 -0500

    Removed routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-  Navigate,
-} from "react-router-dom";
 import "./App.css";
 import MultiStepForm from "./components/MultiStepForm";
-import RootLayout from "./components/RootLayout";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route
-        path={import.meta.env.DEV ? "/*" : "/fem-multi-step-form/*"}
-        element={<RootLayout />}
-        errorElement={<ErrorBoundary />}
-      >
-        <Route index element={<MultiStepForm />} />
-      </Route>
-    )
-  );
-
-  return <RouterProvider router={router} />;
+  return <MultiStepForm />;
 }
 
 export default App;
